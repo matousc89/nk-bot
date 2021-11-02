2cfbb067dd609e7a53ec0a22d39f5230b219eeab
Your Name <you@example.com>
2021-11-02 11:43:15 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# bot.py
+import os
+
+import discord
+
+from SECRET import TOKEN, GUILD
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+client.run(TOKEN)
