2c56470d0b870ccf38e2c7cc4f59ba067e82d38a
Your Name <you@example.com>
2021-11-09 14:36:06 +0100

    covid changes

--- modules/covid/covid.py
+++ modules/covid/covid.py
@@ -15,57 +15,64 @@ def download_data(data_path):
     dataframe.to_pickle(data_path)
     return dataframe
 
-def get_actual_data():
-    """
-    :return: (bool, dataframe) - bool value represent whether the data are new
-    """
-    DATA_PATH = "data.pckl"
-    yesterday_date = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+def handle_fig(func):
+    def wrapper(*args, **kwargs):
+        fig = func(*args, **kwargs)
+        if "filename" in kwargs:
+            filepath = os.path.join(FIG_PATH, kwargs["filename"])
+            fig.savefig(filepath, dpi=100)
+        if not kwargs["display"]:
+            plt.close(fig)
+            fig.clf()
+    return wrapper
 
-    # try to load data
-    try:
-        dataframe = pd.read_pickle(DATA_PATH)
-    except:
-        return True, download_data(DATA_PATH)
-
-    if dataframe.index[-1] == yesterday_date:
-        # data are actual, but already posted
-        return False, dataframe
-    else:
-        dataframe = download_data(DATA_PATH)
-        if dataframe.index[-1] == yesterday_date:
-            # new data are actual, should be posted
-            return True, dataframe
-        else:
-            # new data are not actual
-            return False, dataframe
-
-def plot_data(dataframe, display=False):
+@handle_fig
+def plot_data1(dataframe, **kwargs):
     FIGSIZE = (10, 5)
+    WINDOWNAME = "Test 1"
     tick_spacing = 50
-    fig, ax = plt.subplots(1,1)
-    fig.set_size_inches(*FIGSIZE)
+    fig = plt.figure(WINDOWNAME, figsize=FIGSIZE)
+    ax = plt.gca()
     ax.plot(dataframe.index, dataframe["prirustkovy_pocet_nakazenych"], label="Nově nakažení")
     ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+    for name, date in DATES.items():
+        plt.axvline(dataframe.index.get_loc(date), color="r")
     plt.xticks(rotation=90)
-    plt.tight_layout()
     plt.grid()
-    filepath = os.path.join("modules", "covid", "figs", "plot1.png")
-    fig.savefig(filepath, dpi=100)
-    if display:
-        plt.show()
-    plt.clf()
+    plt.tight_layout()
+    return fig
+
+@handle_fig
+def plot_data2(dataframe, filename=False, display=False):
+    FIGSIZE = (12, 4)
+    WINDOWNAME = "Test 2"
+    subset = dataframe.loc[dataframe.index > DATES["breakpoint1"]]
+    tick_spacing = 1
+    fig = plt.figure(WINDOWNAME, figsize=FIGSIZE)
+    ax = plt.gca()
+    ax.plot(subset.index, subset["kumulativni_pocet_umrti"], label="Kumulativní počet úmrtí")
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+    plt.xticks(rotation=90)
+    plt.grid()
+    plt.tight_layout()
+    return fig
 
 
-def make_covid_report_when_new():
-    new, dataframe = get_actual_data()
-    plot_data(dataframe, display=False)
-    return new
+FIG_PATH = os.path.join("figs")
 
+DATES = {
+    "breakpoint1": "2021-10-01",
+    "the_other_day": "2020-06-25",
+}
 
 if __name__ == "__main__":
-    new, dataframe = get_actual_data()
-    plot_data(dataframe, display=True)
+
+    dataframe = download_data(data_path="data.pckl")
+
+    plot_data1(dataframe, display=True, filename="plot1.png")
+    plot_data2(dataframe, display=True)
+
+    plt.show()
 
 
 
